43537fd94e164695aca130fce8a2fd9cb9a79231
muhayminul <muhayminul96@gmail.com>
2022-05-06 22:30:47 +0600

    data insert and get are success

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 require("dotenv").config();
 const app = express();
@@ -15,11 +15,37 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-client.connect((err) => {
-  const itemsCollection = client.db("SportHouse").collection("Items");
-  // perform actions on the collection object
-  console.log("conneted");
-});
+const itemsCollection = client.db("SportHouse").collection("Items");
+
+async function run() {
+  try {
+    await client.connect();
+
+    app.get("/items", async (req, res) => {
+      const query = {};
+      const cursor = itemsCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.post("/items", async (req, res) => {
+      const newItem  = req.body
+      const result = await itemsCollection.insertOne(newItem);
+      res.send(result);
+    });
+    app.get("/item/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const item = await itemsCollection.findOne(query);
+      res.send(item);
+    });
+
+    // print a message if no documents were found
+  } finally {
+    // await client.close();
+  }
+}
+
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("hello man");
