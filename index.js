9c93a3c21e9b65aae15fce92d487143adfe01356
muhayminul <muhayminul96@gmail.com>
2022-05-07 01:09:51 +0600

    data deleted

--- index.js
+++ index.js
@@ -38,6 +38,13 @@ async function run() {
       const item = await itemsCollection.findOne(query);
       res.send(item);
     });
+    app.delete("/item/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await itemsCollection.deleteOne(query);
+      res.send(result)
+      
+    });
 
     // print a message if no documents were found
   } finally {
